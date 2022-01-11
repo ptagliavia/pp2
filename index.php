7438952660589ba8e7214d9ecf801879cb3df65a
Pat Tagliavia <pat.tagliavia@gmail.com>
2022-01-11 17:13:18 -0500

    Step 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Pair Program 2
+ * Pat Tagliavia
+ * 01/11/2021
+ * URL: https://ptagliavia.greenriverdev.com/SDEV328/pp2/index.php
+*/
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<?php
+echo 'PHP Array practice';
+?>
+</body>
+</html>
